a6e133b7317965d59095c8f98dcc1bfb19598d51
Aleksander Jaworski <aleck156@gmail.com>
2020-03-17 14:17:51 +0100

    included all Job objects in home view

--- portfolio_project/jobs/views.py
+++ portfolio_project/jobs/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from .models import Job
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'jobs/home.html')
+    # get all Job objects from database and turn them to python objects
+    jobs = Job.objects
+    return render(request, 'jobs/home.html',{'jobs' : jobs})
