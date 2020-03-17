60ef727127dece5fd49ac354d450367760885ba8
Aleksander Jaworski <aleck156@gmail.com>
2020-03-17 13:33:38 +0100

    jobs.home.html template now is an Album site template

--- portfolio_project/blog/models.py
+++ portfolio_project/blog/models.py
@@ -5,7 +5,12 @@ class Blog(models.Model):
     title = models.CharField(max_length=50)
     pub_date = models.DateTimeField(auto_now=False, auto_now_add=False)
     message = models.CharField(max_length=250)
-    image = models.ImageField(upload_to='images/', height_field=None, width_field=None, max_length=None)
+    image = models.ImageField(
+        upload_to='images/',
+        height_field=None, 
+        width_field=None, 
+        max_length=None
+        )
 
     def __str__(self):
         return self.title
