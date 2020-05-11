1a27231d009f7da9b412830889c948db6d581f10
Aleksander Jaworski <aleck156@gmail.com>
2020-05-11 10:18:39 +0200

    wrong move!

--- portfolio_project/portfolio_site/urls.py
+++ portfolio_project/portfolio_site/urls.py
@@ -17,14 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.defaults import page_not_found
-
-
 from jobs import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('', django.views.defaults.page_not_found(template_name='home')),
     path('blog/', include('blog.urls', namespace='blog'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
