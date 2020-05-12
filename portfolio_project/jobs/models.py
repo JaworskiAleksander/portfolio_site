21ce53841fd8e5524fe12d74f1dc2abbe365b94c
Aleksander Jaworski <aleck156@gmail.com>
2020-05-12 17:02:11 +0200

    added comment in models.py

--- portfolio_project/jobs/models.py
+++ portfolio_project/jobs/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Job(models.Model):
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
+    ## add 2 fields
+    # Live - link to a live version available online
+    # code - link to a github page with full source code
 
     def __str__(self):
         return self.summary
