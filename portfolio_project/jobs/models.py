1df8ee3a271e1b27f138b6bccfc05c67d537c5ed
Aleksander Jaworski <aleck156@gmail.com>
2020-05-12 18:20:47 +0200

    added URLField arguments

--- portfolio_project/jobs/models.py
+++ portfolio_project/jobs/models.py
@@ -6,9 +6,9 @@ class Job(models.Model):
     summary = models.CharField(max_length=200)
     ## add 2 fields
     # Live - link to a live version available online
-    live_app_link = models.URLField(max_length=250)
+    live_app_link = models.URLField(null=True, blank=True, default='', max_length=250)
     # code - link to a github page with full source code
-    code_source_link = models.URLField(max_length=250)
+    code_source_link = models.URLField(null=True, blank=True, default='', max_length=250)
 
     def __str__(self):
         return self.summary
