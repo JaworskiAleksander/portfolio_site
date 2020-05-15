7ac859587ff49296e53679af7d4c5a715768cf89
Aleksander Jaworski <aleck156@gmail.com>
2020-05-15 15:58:35 +0200

    added resumee logo

--- portfolio_project/static/settings.py
+++ portfolio_project/static/settings.py
@@ -139,3 +139,9 @@ STATICFILES_DIRS=[
 
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
+
+try:
+    from .local_settings import *
+except ImportError:
+    print('ImportError occured')
+    pass
